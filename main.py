d55ea1fe18926fa67abf2fb53fbf78fecd2c3637
Tagirss <seidovtagir@gmail.com>
2022-10-05 22:46:21 +0300

    learning how to use class

---
+++ main.py
@@ -0,0 +1,14 @@
+class Animals():
+    def __init__(self,weight,height,age, sex,speed,jump):
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.sex = sex
+        self.speed = speed
+        self.jump = jump
+    def walk(self):
+        print('скорость', self.speed)
+Rabbit = Animals (weight=80, height = 213, sex='male', age=9, speed = 2, jump = 1)
+print(Rabbit.age)
+print(Rabbit.jump)
+Rabbit.walk()
